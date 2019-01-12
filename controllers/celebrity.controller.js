a3a983760f413114e29baee4fda22efa86cbade7
Cdelatorre <carlosdelatorrediaz@gmail.com>
2019-01-12 16:12:32 +0100

    delete done started edition

--- controllers/celebrity.controller.js
+++ controllers/celebrity.controller.js
@@ -21,3 +21,29 @@ module.exports.get = (req, res, next) => {
   .then(celebrity => res.render('celebrities/detail', { celebrity }))
   .catch(error => console.error('And error has ocurred', error))
 }
+
+module.exports.delete = (req, res, next) => {
+  Celebrity.findByIdAndDelete(req.params.id)
+    .then(celebrity => res.redirect('/celebrities'));
+}
+
+module.exports.edit = (req, res, next) => {
+  Promise.all([
+    User.find(),
+    Book.findById(req.params.id)
+  ])
+  .then((results) => {
+    const users = results[0];
+    const book = results[1]
+    res.render('books/form', { book, users })
+  })
+}
+
+module.exports.doEdit = (req, res, next) => {
+  Book.findById(req.params.id)
+    .then((book) => {
+      book.set(req.body);
+      book.save()
+        .then((book) => { res.redirect('/books' )});
+    })
+}
