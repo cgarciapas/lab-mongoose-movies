8fd69941b739f2620b423b2b5ce258ffbb3ca1a4
Cdelatorre <carlosdelatorrediaz@gmail.com>
2019-01-12 15:08:44 +0100

    routes done

---
+++ routes/celebrities.routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router()
+
+const celebritiesController = require('../controllers/users.controller');
+
+router.get('/create', celebritiesController.create)
+router.post('/create', celebritiesController.doCreate)
+
+module.exports = router;
